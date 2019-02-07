4891d8a23d78d40384763d4b78fc836218b5712c
kirtimisal <kirti.misal@perficient.com>
2019-02-07 17:32:29 +0530

    register information saved in .csv file

--- UserApplication/src/main/java/com/perfi/UserApplication/UserApplication.java
+++ UserApplication/src/main/java/com/perfi/UserApplication/UserApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.perfi.UserApplication"})
 public class UserApplication extends SpringBootServletInitializer {
 
 	@Override
